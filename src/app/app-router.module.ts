a05f909defb9df991830f4e30648ffccb4bf4d5f
rdfenix <rhoodsk8erboy@gmail.com>
2019-03-27 08:43:42 -0300

    routes created

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router'
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRouterModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { BottomComponent } from './bottom/bottom.component';
 import { NavComponent } from './header/nav/nav.component';
 import { CardComponent } from './card/card.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CardComponent } from './card/card.component';
     HeaderComponent,
     BottomComponent,
     NavComponent,
-    CardComponent
+    CardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
