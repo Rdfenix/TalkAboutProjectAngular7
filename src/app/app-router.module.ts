952d0aefd7d7396cb1266ab4f827ca2c78c4ce4e
rdfenix <rhoodsk8erboy@gmail.com>
2019-03-27 16:24:23 -0300

    adjust in login screen

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: '', pathMatch: 'full' }
 ]
 
