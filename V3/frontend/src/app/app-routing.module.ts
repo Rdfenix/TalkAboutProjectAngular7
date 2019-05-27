50fd64bbdc259ac32d2538027afdce43947e7f7f
rdfenix <rhoodsk8erboy@gmail.com>
2019-05-27 14:07:24 -0300

    adding routlink

--- V3/frontend/src/app/app-routing.module.ts
+++ V3/frontend/src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'home', loadChildren: './home/home.module#HomeModule' },
   { path: 'create-post', loadChildren: './create-post/create-post.module#CreatePostModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginRoutingModule' },
-  { path: 'register', loadChildren: './register/register.module#RegisterRoutingModule' }
+  { path: 'login', loadChildren: './login/login.module#LoginModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterModule' }
 ];
 
 @NgModule({
