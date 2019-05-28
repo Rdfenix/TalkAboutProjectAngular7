507faf93ec24248284f5e01dae51eca6d5bfad69
rdfenix <rhoodsk8erboy@gmail.com>
2019-05-28 16:29:42 -0300

    functions of data service created

---
+++ V3/frontend/src/app/service/api.ts
@@ -0,0 +1,1 @@
+export const API_TALK = 'http://localhost:3000'
--- V3/frontend/src/app/service/data.service.ts
+++ V3/frontend/src/app/service/data.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { API_TALK } from './api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllPosts = () => this.http.get(`${API_TALK}/posts`)
+  getUser = (id: string) => this.http.get(`${API_TALK}/users/${id}`)
+  getPost = (id: number) => this.http.get(`${API_TALK}/posts/${id}`)
+  userLogin = (email: string, password: string) => this.http.get(`${API_TALK}/users?email=${email}&password=${password}`)
+  createUser = (data: object) => this.http.post(`${API_TALK}/users`, data)
+  createPost = (data: object) => this.http.post(`${API_TALK}/posts`, data)
+  getAllComments = (id: number) => this.http.get(`${API_TALK}/comments?postID=${id}`)
+  createComment = (data: object) => this.http.post(`${API_TALK}/comments`, data)
+
 }
