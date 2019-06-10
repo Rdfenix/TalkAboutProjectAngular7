3d0f5cf8da941b75656c14f094dfcf99ac764376
rdfenix <rhoodsk8erboy@gmail.com>
2019-06-10 14:32:02 -0300

    refactoring in css

--- V3/frontend/src/app/home/comment/comment.component.ts
+++ V3/frontend/src/app/home/comment/comment.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Comment } from 'src/app/model/comment';
+import { DataService } from 'src/app/service/data.service';
+import { validateIsEmpty } from 'src/app/utils/utils';
+import { User } from 'src/app/model/user';
+import { Post } from 'src/app/model/post';
+
 
 @Component({
   selector: 'app-comment',
@@ -7,9 +14,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private dataService: DataService) { }
+
+  id: number = this.route.snapshot.params['id']
+  title: string
+  comments: Comment[] = []
+  formData: Comment = {
+    commentary: ""
+  }
+
+  loadComments = () => this.dataService.getAllComments(this.id).subscribe((response: Comment[]) => {
+    if (!validateIsEmpty(response))
+      response.map(item => this.dataService.getUser(item.userID).subscribe((user: User) => {
+        let userName = user.name + ' ' + user.lastName
+        this.comments.push({ ...item, userName: userName })
+      }))
+  })
+
+  getTitleComment = () => this.dataService.getPost(this.id).subscribe((response: Post) => this.title = response.title)
 
   ngOnInit() {
+    this.loadComments()
+    this.getTitleComment()
   }
 
 }
